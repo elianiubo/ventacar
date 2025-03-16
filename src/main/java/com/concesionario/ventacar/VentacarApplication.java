819fd114f67613b6190a598d5fba18831dd53377
Carlos <solrak_27@hotmail.com>
2025-03-16 22:28:34 +0100

    parte del backend del buscador

---
+++ src/main/java/com/concesionario/ventacar/VentacarApplication.java
@@ -0,0 +1,13 @@
+package com.concesionario.ventacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentacarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentacarApplication.class, args);
+	}
+
+}
